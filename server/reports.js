1de0fe4628d02a88959395217d7c2a78baa08feb
Aman Jha <gunujha@gmail.com>
2021-09-20 14:02:17 -0400

    can replace any individual report with a new object

--- server/reports.js
+++ server/reports.js
@@ -12,21 +12,21 @@ const readFile = (callback) => {
   });
 };
 
-// GET: /reports
+// GET: /reports - grab all reports
 exports.getAll = (req, res) => {
   readFile((data) => {
     res.send(data.elements);
   });
 };
 
-// GET: /reports/:reportId
+// GET: /reports/:reportId - grab one report by id
 exports.getOne = (req, res) => {
   readFile((data) => {
     res.send(data.elements.find((r) => r.id === req.params.reportId));
   });
 };
 
-// PUT: /update
+// PUT: /update (this doesn't do anything except paste the db on top of itself)
 exports.updateOne = (req, res) => {
   readFile((data) => {
     fs.writeFile(filepath, JSON.stringify(data, null, 2), encoding, (err) => {
@@ -36,10 +36,16 @@ exports.updateOne = (req, res) => {
   });
 };
 
+// PUT: /block/:reportId - mark a report as blocked so it can't be viewed
 exports.block = (req, res) => {
   readFile((data) => {
     const id = req.params.reportId;
-    const report = data.elements.filter((r) => r.id === id);
-    console.log(report);
+    const idx = data.elements.findIndex((r) => r.id === id);
+    data.elements[idx] = { message: "aman should get this job" };
+
+    fs.writeFile(filepath, JSON.stringify(data, null, 2), encoding, (err) => {
+      if (err) throw err;
+      res.send(data.elements[idx]);
+    });
   });
 };
