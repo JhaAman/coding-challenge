2df84e66e1d73c4d2aff8b5542157e8b0b9c2e66
Aman Jha <gunujha@gmail.com>
2021-09-20 14:37:27 -0400

    change null to [] to simulate loading...

--- client/src/components/Report.js
+++ client/src/components/Report.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Report = () => {
+const Report = ({ id }) => {
   return (
     <div>
-      <h3>ID: </h3>
+      <h3>ID: {id}</h3>
     </div>
   );
 };
--- client/src/components/ReportList.js
+++ client/src/components/ReportList.js
@@ -1,27 +1,26 @@
 import React, { useState, useEffect } from "react";
-import Report from "./Report";
 import axios from "axios";
+
+import Report from "./Report";
 
 const path = "http://localhost:8000";
 
 const ReportList = () => {
-  const [_reports, set_reports] = useState(null);
+  const [_reports, set_reports] = useState([]);
 
   useEffect(() => {
     axios.get(path + "/reports/").then((res) => {
       set_reports(res.data);
     });
-  });
+  }, []);
 
   return (
     <div>
-      <h2>Current Reports:</h2>
+      <h2>Current Reports: {_reports.length}</h2>
       <Report />
-      <p>
-        {_reports.map((r) => {
-          return <Report />;
-        })}
-      </p>
+      {_reports.map((r) => {
+        return <Report key={r.id} id={r.id} />;
+      })}
     </div>
   );
 };
