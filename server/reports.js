b913dcba6761a73291ab43314b5b94eec6df90cd
Aman Jha <gunujha@gmail.com>
2021-09-20 13:39:30 -0400

    lets abstract readFile in order to use for updating

--- server/reports.js
+++ server/reports.js
@@ -3,12 +3,20 @@ const fs = require("fs");
 const filepath = "./data/reports.json";
 const encoding = "utf-8";
 
-exports.getAll = (req, res) => {
+const readFile = () => {
   fs.readFile(filepath, encoding, (err, data) => {
     if (err) throw err;
-    // Send response to insomnia (or client)
-    res.send(JSON.parse(data).elements);
+    return JSON.parse(data);
   });
+};
+
+exports.getAll = (req, res) => {
+  // fs.readFile(filepath, encoding, (err, data) => {
+  //   if (err) throw err;
+  //   // Send response to insomnia (or client)
+  //   res.send(JSON.parse(data).elements);
+  // });
+  res.send(readFile().elements);
 };
 
 exports.getOne = (req, res) => {
