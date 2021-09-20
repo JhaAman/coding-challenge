8f521687efae457a9ff597e4de1f2de7e469e93a
Aman Jha <gunujha@gmail.com>
2021-09-20 14:04:06 -0400

    abstracted out writeFile for future use

--- server/reports.js
+++ server/reports.js
@@ -12,6 +12,13 @@ const readFile = (callback) => {
   });
 };
 
+const writeFile = (newData, callback) => {
+  fs.writeFile(filepath, JSON.stringify(newData, null, 2), encoding, (err) => {
+    if (err) throw err;
+    callback();
+  });
+};
+
 // GET: /reports - grab all reports
 exports.getAll = (req, res) => {
   readFile((data) => {
@@ -43,8 +50,7 @@ exports.block = (req, res) => {
     const idx = data.elements.findIndex((r) => r.id === id);
     data.elements[idx] = { message: "aman should get this job" };
 
-    fs.writeFile(filepath, JSON.stringify(data, null, 2), encoding, (err) => {
-      if (err) throw err;
+    writeFile(data, () => {
       res.send(data.elements[idx]);
     });
   });
