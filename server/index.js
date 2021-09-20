fac2e67c722126a7ff51bcc51e047933090a8952
Aman Jha <gunujha@gmail.com>
2021-09-20 12:56:05 -0400

    trying out readfile

--- server/index.js
+++ server/index.js
@@ -4,13 +4,19 @@ const cors = require("cors");
 
 const app = express();
 const port = 8000;
+const filepath = "./data/reports.json";
+const encoding = "utf-8";
 
 // Allows us to use JSON as a db
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// GET: all reports using fs.readFile()
 app.get("/", (req, res) => {
-  fs.readFile();
+  fs.readFile(filepath, encoding, (err, data) => {
+    if (err) throw err;
+    res.send(JSON.parse(data));
+  });
 });
 
 app.listen(port);
