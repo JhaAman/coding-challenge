7ffcd1315c023ae598310de5488ead5b494fb794
Aman Jha <gunujha@gmail.com>
2021-09-20 14:43:37 -0400

    organization + add buttons (no func.)

--- client/src/components/Report.js
+++ client/src/components/Report.js
@@ -2,13 +2,19 @@ import React from "react";
 
 const Report = ({ id, state, type, message }) => {
   return (
-    <div>
-      <p>Id: {id}</p>
-      <p>State: {state}</p>
-      <a href="/">Details</a>
-      <div>
+    <div className="container">
+      <div className="left">
+        <p>Id: {id}</p>
+        <p>State: {state}</p>
+        <a href="/">Details</a>
+      </div>
+      <div className="middle">
         <p>Type: {type}</p>
         <p>Message: {message}</p>
+      </div>
+      <div className="right">
+        <button>Block</button>
+        <button>Resolve</button>
       </div>
     </div>
   );
