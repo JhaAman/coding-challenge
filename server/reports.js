8cdd7228e6e24675e391861c7e8c733f0e8cfd26
Aman Jha <gunujha@gmail.com>
2021-09-20 13:48:18 -0400

    adapting readfile for update

--- server/reports.js
+++ server/reports.js
@@ -11,29 +11,26 @@ const readFile = (callback) => {
 };
 
 exports.getAll = (req, res) => {
-  // fs.readFile(filepath, encoding, (err, data) => {
-  //   if (err) throw err;
-  //   // Send response to insomnia (or client)
-  //   res.send(JSON.parse(data).elements);
-  // });
   readFile((data) => {
-    // console.log(data);
     res.send(data.elements);
   });
 };
 
 exports.getOne = (req, res) => {
-  fs.readFile(filepath, encoding, (err, data) => {
-    if (err) throw err;
-    dataParsed = JSON.parse(data);
-    res.send(dataParsed.elements.find((r) => r.id === req.params.reportId));
+  readFile((data) => {
+    res.send(data.elements.find((r) => r.id === req.params.reportId));
   });
 };
 
 exports.updateOne = (req, res) => {
-  oldData = this.getAll(req, res);
-  fs.writeFile(filepath, JSON.stringify(oldData), encoding, (err) => {
-    if (err) throw err;
-    res.send(200);
+  let oldData = [];
+  readFile((data) => {
+    oldData = data.elements;
+    console.log(data);
   });
+
+  // fs.writeFile(filepath, JSON.stringify(oldData), encoding, (err) => {
+  //   if (err) throw err;
+  //   res.send(200);
+  // });
 };
